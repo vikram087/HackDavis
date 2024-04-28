fbe88afd069bc02b93ac468da6065664b15136ec
savio-esmailzadeh <savio.esmailzadeh@gmail.com>
2024-04-28 06:50:12 -0700

    probably undid overwrite

--- hackdavis/frontend/src/app/scan/page.tsx
+++ hackdavis/frontend/src/app/scan/page.tsx
@@ -1,91 +1,252 @@
-// 'use client'
+'use client'
+import React, { useState, useRef } from 'react'
 
+import ReactCrop, {
+  centerCrop,
+  makeAspectCrop,
+  Crop,
+  PixelCrop,
+  convertToPixelCrop,
+} from 'react-image-crop'
+import { canvasPreview } from './canvasPreview'
+import { useDebounceEffect } from './useDebounceEffect'
 
-// import { MultiValue } from 'react-select/animated'
-// import Header from '../components/header';
-// import { AuthProvider } from '@propelauth/react';
-// import React, { useEffect, useState } from 'react';
+import 'react-image-crop/dist/ReactCrop.css'
 
-// export default function PastScans() {
-//   const [response, setResponse] = useState<any>([]);
+// This is to demonstate how to make and center a % aspect crop
+// which is a bit trickier so we use some helper functions.
+function centerAspectCrop(
+  mediaWidth: number,
+  mediaHeight: number,
+  aspect: number,
+) {
+  return centerCrop(
+    makeAspectCrop(
+      {
+        unit: '%',
+        width: 90,
+      },
+      aspect,
+      mediaWidth,
+      mediaHeight,
+    ),
+    mediaWidth,
+    mediaHeight,
+  )
+}
 
-//   const getItem = (id: string) => {
-//     fetch(`http://localhost:8080/api/getItem`, {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({"barcode": id}),
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//     //   console.log(data);
-//       setResponse((prevResponse: any) => [...prevResponse, data.document]);
-//     })
-//     .catch (error => {
-//       console.error("Could not fetch item");
-//     })
-//   }
+export default function Capture({ image }: { image: any }) {
+  const [imgSrc, setImgSrc] = useState(image)
+  const previewCanvasRef = useRef<HTMLCanvasElement>(null)
+  const imgRef = useRef<HTMLImageElement>(null)
+  const hiddenAnchorRef = useRef<HTMLAnchorElement>(null)
+  const blobUrlRef = useRef('')
+  const [crop, setCrop] = useState<Crop>()
+  const [completedCrop, setCompletedCrop] = useState<PixelCrop>()
+  const [scale, setScale] = useState(1)
+  const [rotate, setRotate] = useState(0)
+  const [aspect, setAspect] = useState<number | undefined>(16 / 9)
 
-//   const loadFromLocalStorage = (key: string) => {
-//     try {
-//       const serializedValue = localStorage.getItem(key);
-//       if (serializedValue === null) {
-//         return undefined;
-//       }
-//       return JSON.parse(serializedValue);
-//     } catch (error) {
-//       console.error('Error reading from localStorage', error);
-//       return undefined;
-//     }
-//   };
+  function onSelectFile(e: React.ChangeEvent<HTMLInputElement>) {
+    if (e.target.files && e.target.files.length > 0) {
+      setCrop(undefined) // Makes crop preview update between images.
+      const reader = new FileReader()
+      reader.addEventListener('load', () =>
+        setImgSrc(reader.result?.toString() || ''),
+      )
+      reader.readAsDataURL(e.target.files[0])
+    }
+  }
 
-//     const fetchData = () => {
-//         fetch(`http://localhost:8080/api/pastScans`, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify({"username": loadFromLocalStorage("username")}),
-//         })
-//         .then(response => response.json())
-//         .then(data => {
-//             const pastScans = data.pastScans;
-//             pastScans.map(async (id: string) => {
-//                 getItem(id);
-//             });
-//         })
-//         .catch (error => {
-//         console.error("Could not fetch item");
-//         })
-//     }
+  function onImageLoad(e: React.SyntheticEvent<HTMLImageElement>) {
+    if (aspect) {
+      const { width, height } = e.currentTarget
+      setCrop(centerAspectCrop(width, height, aspect))
+    }
+  }
+  async function onDownloadCropClick() {
+    const image = imgRef.current;
+    const previewCanvas = previewCanvasRef.current;
+    if (!image || !previewCanvas || !completedCrop) {
+      throw new Error('Crop canvas does not exist');
+    }
+  
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+  
+    const offscreen = new OffscreenCanvas(
+      completedCrop.width * scaleX,
+      completedCrop.height * scaleY,
+    );
+    const ctx = offscreen.getContext('2d');
+    if (!ctx) {
+      throw new Error('No 2d context');
+    }
+  
+    ctx.drawImage(
+      previewCanvas,
+      0,
+      0,
+      previewCanvas.width,
+      previewCanvas.height,
+      0,
+      0,
+      offscreen.width,
+      offscreen.height,
+    );
+  
+    const blob = await offscreen.convertToBlob({
+      type: 'image/png',
+    });
+  
+    // Create FormData object to send the image data
+    const formData = new FormData();
+    formData.append('image', blob);
+  
+    // Send the image data to the Flask backend using fetch
+    fetch('', {
+      method: 'POST',
+      body: formData,
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to upload image');
+      }
+      return response.json(); // Assuming the Flask backend returns JSON response
+    })
+    .then(data => {
+      // Handle response from Flask backend if needed
+      console.log('Upload successful', data);
+    })
+    .catch(error => {
+      console.error('Error uploading image', error);
+    });
+  }
 
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
+  useDebounceEffect(
+    async () => {
+      if (
+        completedCrop?.width &&
+        completedCrop?.height &&
+        imgRef.current &&
+        previewCanvasRef.current
+      ) {
+        // We use canvasPreview as it's much faster than imgPreview.
+        canvasPreview(
+          imgRef.current,
+          previewCanvasRef.current,
+          completedCrop,
+          scale,
+          rotate,
+        )
+      }
+    },
+    100,
+    [completedCrop, scale, rotate],
+  )
 
-//   const items = response.map((item: any, index: number) => (
-//     <div className='border border-black rounded-lg'>
-//         <p>{item.allergens}</p>
-//         <p>{item.name}</p>
-//         <p>{item.barcode}</p>
-//     </div>
-//   ));
+  function handleToggleAspectClick() {
+    if (aspect) {
+      setAspect(undefined)
+    } else {
+      setAspect(16 / 9)
 
-//   return (
-//     <AuthProvider authUrl='https://6961223141.propelauthtest.com'>
-//       <div>
-//         <Header />
-//         <div className="flex flex-col items-center justify-start h-screen">
-//           <h1 className="text-3xl font-bold mb-8 p-[2%]">Past Scans</h1>
-//           <div className="w-1/5"> {/* 20% of the viewport width */}
-//             {items}
-//           </div>
-//         </div>
-//       </div>
-//     </AuthProvider>
-//   );
-// }
+      if (imgRef.current) {
+        const { width, height } = imgRef.current
+        const newCrop = centerAspectCrop(width, height, 16 / 9)
+        setCrop(newCrop)
+        // Updates the preview
+        setCompletedCrop(convertToPixelCrop(newCrop, width, height))
+      }
+    }
+  }
 
-
-// export default WebcamCapturePage;
+  return (
+    <div className="App">
+      <div className="Crop-Controls">
+        <input type="file" accept="image/*" onChange={onSelectFile} />
+        <div>
+          <label htmlFor="scale-input">Scale: </label>
+          <input
+            id="scale-input"
+            type="number"
+            step="0.1"
+            value={scale}
+            disabled={!imgSrc}
+            onChange={(e) => setScale(Number(e.target.value))}
+          />
+        </div>
+        <div>
+          <label htmlFor="rotate-input">Rotate: </label>
+          <input
+            id="rotate-input"
+            type="number"
+            value={rotate}
+            disabled={!imgSrc}
+            onChange={(e) =>
+              setRotate(Math.min(180, Math.max(-180, Number(e.target.value))))
+            }
+          />
+        </div>
+        <div>
+          <button onClick={handleToggleAspectClick}>
+            Toggle aspect {aspect ? 'off' : 'on'}
+          </button>
+        </div>
+      </div>
+      {!!imgSrc && (
+        <ReactCrop
+          crop={crop}
+          onChange={(_, percentCrop) => setCrop(percentCrop)}
+          onComplete={(c) => setCompletedCrop(c)}
+          aspect={aspect}
+          // minWidth={400}
+          minHeight={100}
+          // circularCrop
+        >
+          <img
+            ref={imgRef}
+            alt="Crop me"
+            src={imgSrc}
+            style={{ transform: `scale(${scale}) rotate(${rotate}deg)` }}
+            onLoad={onImageLoad}
+          />
+        </ReactCrop>
+      )}
+      {!!completedCrop && (
+        <>
+          <div>
+            <canvas
+              ref={previewCanvasRef}
+              style={{
+                border: '1px solid black',
+                objectFit: 'contain',
+                width: completedCrop.width,
+                height: completedCrop.height,
+              }}
+            />
+          </div>
+          <div>
+            <button onClick={onDownloadCropClick}>Send Crop</button>
+            <div style={{ fontSize: 12, color: '#666' }}>
+              If you get a security error when downloading try opening the
+              Preview in a new tab (icon near top right).
+            </div>
+            <a
+              href="#hidden"
+              ref={hiddenAnchorRef}
+              download
+              style={{
+                position: 'absolute',
+                top: '-200vh',
+                visibility: 'hidden',
+              }}
+            >
+              Hidden download
+            </a>
+          </div>
+        </>
+      )}
+    </div>
+  )
+}
