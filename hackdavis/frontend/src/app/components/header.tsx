07d395752f82e01972e5c273f998695af6bf5370
Vikram Penumarti <vikram.penumarti@gmail.com>
2024-04-28 12:06:33 -0700

    Update header.tsx

--- hackdavis/frontend/src/app/components/header.tsx
+++ hackdavis/frontend/src/app/components/header.tsx
@@ -118,7 +118,7 @@ const Header = withAuthInfo((props: WithAuthInfoProps) => {
     const underLine = <div className="pt-0.5 absolute w-full h-0.5 bg-white/80 scale-x-0 group-hover:scale-x-100 transition-transform origin-left "/>
     return (
         <nav className="pl-8 pr-8 whitespace-nowrap flex items-center justify-between bg-sky-950 text-white h-24">
-            <button onClick={goHome} className="text-yellow-500 text-5xl hover:text-gray-300">ProjectName</button>
+            <button onClick={goHome} className="text-yellow-500 text-5xl hover:text-gray-300">AllerSense</button>
             <div className="flex items-center space-x-24 justify-end"> {/* Adjusted justify-end here */}
                 <button className="hover:text-yellow-500 font-mono hover:opacity-100 ease-in-out relative group duration-500" onClick={goToPastScans}>Past Scans {underLine}</button> 
                 <button className="hover:text-yellow-500 font-mono hover:opacity-100 ease-in-out relative group duration-500" onClick={goToScan}>Scan an Item {underLine}</button> 
